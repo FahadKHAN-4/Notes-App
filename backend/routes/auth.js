ec147d894851d99e4f917987f80d3431df26ee7a
FahadKHAN-4 <khanfahad0059@gmail.com>
2024-03-23 11:59:22 +0800

    login endpoint created

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,16 +7,15 @@ var jwt = require('jsonwebtoken');
 
 const JWT_SECRET = "fahad123"
 
-
+// Create a user
 router.post('/createuser', body('email').isEmail(),
-    body('name', "Enter a valid name of length > 3.").isLength({ min: 3 }),
-    body('password').isLength({ min: 5 }),
-
-    async (req, res)=>{ 
+    body('name', "Enter a valid name of length > 3.").isLength({ max: 25 }),
+    body('password').isLength({ min: 5 })
+    , async (req, res)=>{ 
 
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+          return res.status(400).json({ errors: errors.array() });
         }
 
         const salt = await bcrypt.genSaltSync(10);
@@ -55,8 +54,51 @@ router.post('/createuser', body('email').isEmail(),
         //     }
         //     res.status(500).json({ errors: [{ msg: 'Server error' }] });
         //   });
-        
+  }
+); 
 
-}); 
+// Authenticate a user
+
+router.post('/login', body('name', "Enter a valid name of length > 3.").isLength({ max: 25 }),
+  body('email').isEmail(),
+  body('password', "Password cannot be blank").exists()
+  , async (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {email, password} = req.body;
+
+    try{
+
+      let user = await User.findOne({email});
+
+      if(!user){
+        return res.status(400).json({ errors: "The email or password is not correct." });
+      }
+
+      const passwordComapre = await bcrypt.compare(password, user.password);
+
+      if(!passwordComapre){
+        return res.status(400).json({ errors: "The email or password is not correct." });
+      }
+
+      const payload = {
+        user:{
+          id: user.id
+        }
+      }
+
+      const authtoken = jwt.sign(payload,JWT_SECRET);
+      res.json({authtoken});
+
+    }catch(error){
+      res.status(500).json({ errors: [{ msg: 'Server error' }] });
+    };
+
+  }
+);
 
 module.exports = router;
