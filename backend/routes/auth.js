7a4199d445526e89ba7cd502489311a3071965a7
FahadKHAN-4 <khanfahad0059@gmail.com>
2024-03-23 12:13:32 +0800

    small refractoring

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,57 +7,45 @@ var jwt = require('jsonwebtoken');
 
 const JWT_SECRET = "fahad123"
 
-// Create a user
-router.post('/createuser', body('email').isEmail(),
-    body('name', "Enter a valid name of length > 3.").isLength({ max: 25 }),
+// Route 1: Create a user
+router.post('/createuser', body('email', "Please enter a valid email").isEmail(),
+    body('name', "Enter a valid name of length < 25.").isLength({ max: 25 }),
     body('password').isLength({ min: 5 })
     , async (req, res)=>{ 
 
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const salt = await bcrypt.genSaltSync(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
+      try {
+        const user = await User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: secPass,
+        });
+
+        const data = {
+          user:{
+            id: user.id
+          }
         }
 
-        const salt = await bcrypt.genSaltSync(10);
-        const secPass = await bcrypt.hash(req.body.password, salt);
-
-        try {
-            // Create the user using await to wait for the Promise to resolve
-            const user = await User.create({
-              name: req.body.name,
-              email: req.body.email,
-              password: secPass,
-            });
-
-            const data = {
-                user:{
-                  id: user.id
-                }
-            }
-
-            const authtoken = jwt.sign(data,JWT_SECRET);
-            res.json({authtoken});
-          } catch (err) {
+        const authtoken = jwt.sign(data,JWT_SECRET);
+        res.json({authtoken});
+        } catch (err) {
             if (err.code === 11000) {
               return res.status(400).json({msg:'The email address you have entered is already associated with another account.' });
             }
             res.status(500).json({ errors: [{ msg: 'Server error' }] });
         }
-
-        // User.create({
-        //     name: req.body.name,
-        //     email: req.body.email,
-        //     password: req.body.password,
-        // }).then(user => res.json(user)).catch(err => {
-        //     if (err.code === 11000) {
-        //       return res.status(400).json({ errors: [{ msg: 'The email address you have entered is already associated with another account.' }] });
-        //     }
-        //     res.status(500).json({ errors: [{ msg: 'Server error' }] });
-        //   });
   }
 ); 
 
-// Authenticate a user
+// Routhe 2: Authenticate a user at login
 
 router.post('/login', body('name', "Enter a valid name of length > 3.").isLength({ max: 25 }),
   body('email').isEmail(),
